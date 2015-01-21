65ca638706a1e250728043aab28548fe7ff7faed
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-01-21 09:47:39 +0100

    small test + gitignore

---
+++ test1.py
@@ -0,0 +1,11 @@
+import gzip
+import sys
+import json
+
+def actor():
+    for line in gzip.open('2015-01-01-15.json.gz'):
+        data = json.loads(line.strip())
+        print data["actor"]["id"], data["repo"]["id"]
+
+if __name__ == "__main__":
+    actor()
