8383ddcdfe233290fd2ad47c0c0f675ea3802aff
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-01-21 15:43:48 +0100

    gephi

--- test3.py
+++ test3.py
@@ -23,18 +23,19 @@ def get_bipartite_graph(input_file):
         B.add_node(data_line['repo']['name'], bipartite=1)
         B.add_edge(data_line['actor']['login'], data_line['repo']['name'])
         
-        if i == 1000:
+        if i == 500:
             break
 
     #for n in B.nodes():
     #    if len(nx.node_connected_component(B,n)) < -1: # fix the lowest degree
     #        B.remove_node(n)
 
-    nx.draw_graphviz(B)
+    #nx.draw_graphviz(B,prog='circo')
     #nx.write_dot(B,'file2.dot')
-    plt.show()
+    #plt.show()
+    nx.write_gml(B,'e.gml')
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit('Provide exactly one archive in input')
-    B = get_bipartite_graph(sys.argv[1])
+    get_bipartite_graph(sys.argv[1])
