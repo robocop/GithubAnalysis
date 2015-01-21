4d5d680136182c4288f03defdcfc04523031ca13
Quentin Cormier <quentincormier@gmail.com>
2015-01-21 10:40:45 +0100

    Build the bipartite graph

--- read_data.py
+++ read_data.py
@@ -1,17 +1,25 @@
 import gzip
 import json
+import networkx as nx
+from networkx.algorithms import bipartite
 
 
-def actor():
-    i = 0
+def get_bipartite_graph():
+    B = nx.Graph()
+
     for line in gzip.open('data/2015-01-01-15.json.gz'):
         data_line = json.loads(line.decode('utf8'))
-        if i == 0:
-            print(data_line)
+
+        B.add_node(data_line['actor']['login'], bipartite=0)
+        B.add_node(data_line['repo']['name'], bipartite=1)
+        B.add_edge(data_line['actor']['login'], data_line['repo']['name'])
+
         print('Author = %s, Repo = %s, Action = %s' %
               (data_line['actor']['login'], data_line['repo']['name'], data_line['type']))
-        i += 1
+
+    return B
 
 
 if __name__ == "__main__":
-    actor()
+    B = get_bipartite_graph()
+    nx.draw_networkx(B)
