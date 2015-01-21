935bec8b6b78b136ee9c43dcad9119c56ee270e4
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-01-21 09:56:40 +0100

    provide the archive as input

--- test1.py
+++ test1.py
@@ -2,10 +2,12 @@ import gzip
 import sys
 import json
 
-def actor():
-    for line in gzip.open('2015-01-01-15.json.gz'):
+def actor(input_file):
+    for line in gzip.open(input_file):
         data = json.loads(line.strip())
         print data["actor"]["id"], data["repo"]["id"]
 
 if __name__ == "__main__":
-    actor()
+    if len(sys.argv) != 2:
+        sys.exit('Provide exactly one archive in input')
+    actor(input_file)
