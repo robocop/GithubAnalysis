e663a7139227ef176baf1831974fd5e8ed39b245
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-01-21 11:14:37 +0100

    Plot in a PNG.

--- read_data.py
+++ read_data.py
@@ -3,6 +3,7 @@
 import gzip
 import json
 import networkx as nx
+import matplotlib.pyplot as plt
 import sys
 
 #from networkx.algorithms import bipartite
@@ -10,8 +11,9 @@ import sys
 
 def get_bipartite_graph(input_file):
     B = nx.Graph()
-
+    i=0
     for line in gzip.open(input_file):
+        i+=1
         data_line = json.loads(line.decode('utf8'))
 
         B.add_node(data_line['actor']['login'], bipartite=0)
@@ -20,6 +22,8 @@ def get_bipartite_graph(input_file):
 
         print('Author = %s, Repo = %s, Action = %s' %
               (data_line['actor']['login'], data_line['repo']['name'], data_line['type']))
+        if i==1000:
+            break
 
     return B
 
@@ -27,5 +31,8 @@ def get_bipartite_graph(input_file):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit('Provide exactly one archive in input')
-    B = get_bipartite_graph(sys.argv[1])
-    nx.draw_networkx(B)
+    G = get_bipartite_graph(sys.argv[1])
+    pos=nx.spring_layout(G)
+    nx.draw(G,pos,node_color='#A0CBE2',width=1,edge_cmap=plt.cm.Blues,with_labels=False)
+    plt.savefig("graph.png") # save as png
+
