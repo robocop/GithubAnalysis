46d0a862b46e55fbb49ad22792a7fb7ec245a108
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2015-01-21 09:54:40 +0100

    Python3.

--- test1.py
+++ test1.py
@@ -4,8 +4,8 @@ import json
 
 def actor():
     for line in gzip.open('2015-01-01-15.json.gz'):
-        data = json.loads(line.strip())
-        print data["actor"]["id"], data["repo"]["id"]
+        data = json.loads(line.decode())
+        print(data["actor"]["id"], data["repo"]["id"])
 
 if __name__ == "__main__":
     actor()
