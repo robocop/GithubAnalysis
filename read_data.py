f2bf2db8a894952bcdd3d8bb6ddc72d5136bd99a
Quentin Cormier <quentincormier@gmail.com>
2015-01-21 10:00:46 +0100

    read the actions of a gz file

--- read_data.py
+++ read_data.py
@@ -1,2 +1,17 @@
-if __name__ == '__main__':
-    print("Hello, world!")
+import gzip
+import json
+
+
+def actor():
+    i = 0
+    for line in gzip.open('data/2015-01-01-15.json.gz'):
+        data_line = json.loads(line.decode('utf8'))
+        if i == 0:
+            print(data_line)
+        print('Author = %s, Repo = %s, Action = %s' %
+              (data_line['actor']['login'], data_line['repo']['name'], data_line['type']))
+        i += 1
+
+
+if __name__ == "__main__":
+    actor()
