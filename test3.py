45e2c438619e783ed570af1c23db5dc151d561f4
yhamoudi <yassine.hamoudi@ens-lyon.fr>
2015-01-21 11:34:24 +0100

    bla

--- test3.py
+++ test3.py
@@ -23,18 +23,23 @@ def get_bipartite_graph(input_file):
         B.add_node(data_line['repo']['name'], bipartite=1)
         B.add_edge(data_line['actor']['login'], data_line['repo']['name'])
 
+        rem_list = []
+        
+        for n in B.nodes():
+            if len(nx.node_connected_component(B,n)) < 3:
+                rem_list.append(n)
+
+        B.remove_nodes_from(rem_list)
+
         #print('Author = %s, Repo = %s, Action = %s' % (data_line['actor']['login'], data_line['repo']['name'], data_line['type']))
         
-        if i > 1000:
+        if i > 500:
             break
 
     nx.draw(B)
     plt.show()
-    #return B
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit('Provide exactly one archive in input')
     B = get_bipartite_graph(sys.argv[1])
-    #nx.draw_networkx(B)
